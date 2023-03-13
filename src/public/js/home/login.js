b8ac842af7bc9e79dff04b06367abf20dac25e47
yh9035926 <yh9035926@naver.com>
2023-03-14 07:56:27 +0900

    login

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -1,1 +1,19 @@
-console.log("헬로");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+const login = () => {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: json.stringify(req),
+  });
+};
+
+loginBtn.addEventListener("click", login);
