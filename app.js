d9a02ca45364edebd20070eb7576157524b4e675
yh9035926 <yh9035926@naver.com>
2023-03-14 07:30:20 +0900

    login

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const { Model } = require("sequelize");
 const app = express();
 
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);
 
 module.exports = app;
