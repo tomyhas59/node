003343d49af1e5cc793fe55f4c1cf5bb3bb6bc61
yh9035926 <yh9035926@naver.com>
2023-03-14 12:06:47 +0900

    login

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -13,7 +13,18 @@ const login = () => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
-  });
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(("로그인 중 에러 발생"));
+    });
 };
 
 loginBtn.addEventListener("click", login);
